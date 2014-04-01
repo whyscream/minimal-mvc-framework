e816553377cc1409ce3bca6a18ec6b3ea7317cf2
Tom Hendrikx <tom@whyscream.net>
2014-04-01 22:50:00 +0200

    Add a simple example of how to customize the results.

--- example-app/PiesModel.php
+++ example-app/PiesModel.php
@@ -5,19 +5,30 @@
     /**
      * Simple example of a custom Model class.
      *
-     * This class is just for illustration purposes, it does nothing useful. In stead of:
-     * @code
-     *  $pies = new PiesModel($dsn);
-     * @endcode
-     * You could also have written:
-     * @code
-     *  $pies = new PdoModel($dsn, 'pies');
-     * @endcode
+     * When you know what data is in the Model, you can make custom changes
+     * based on that extra knowledge.
      */
     class PiesModel extends PdoModel {
 
+        const CM_IN_INCHES = 0.393701;
+
+        /**
+         * Set table to use, now that we have context.
+         */
         public function __construct($dsn) {
             parent::__construct($dsn, 'pies');
         }
 
+        /**
+         * Convert result properties to logical datatypes, now that we have context.
+         */
+        public function current() {
+            $data = parent::current();
+            $data->diameter = intval($data->diameter);
+
+            $inches = $data->diameter * $this::CM_IN_INCHES;
+            $data->diameter_in_inches = round($inches, 1);
+
+            return $data;
+        }
     }
